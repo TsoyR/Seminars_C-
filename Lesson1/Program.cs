b47c6f2b79b2f4d7859abdb1e0dc36f0f298e0cb
Roman <roman@MacBook-Air-Roman.local>
2022-08-20 09:31:25 +0900

    Lesson1

---
+++ Lesson1/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Console.Write("Input number: ");
+
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+//Console.WriteLine("The quard of " + number + " is " + kvadrat);
+Console.WriteLine($"The quard of {number} is {kvadrat}");
+*/
+
+Console.Write("Input number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inpur number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine($"{number1} is quard of {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} not is quard of {number2}");
+}
+
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = number * (-1);
+
+while (current < number)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+
+
